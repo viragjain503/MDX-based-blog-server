55695ec8d8ac67bdac148cbb1caf779d7f4b5e4f
viragjain503 <viragjain503@gmail.com>
2023-04-01 13:00:05 +0530

    basic changes

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const port = process.env.PORT || 5000
 
 const articleRoute = require('./routes/article')
 
+//routes
 app.use('/article',articleRoute);
 
 app.use(cors());
 
+// for database connection
 mongoose.connect("mongodb://127.0.0.1:27017/myblog");
 
+// for database connection error
 mongoose.connection.on("error", err => {
   console.log("err", err)
 })
 
+// for database connection successfull
 mongoose.connection.on("connected", (err, res) => {
   console.log("mongoose is connected")
 })
